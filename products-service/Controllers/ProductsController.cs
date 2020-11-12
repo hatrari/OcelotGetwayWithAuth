bb1dd0c0d1b89a23dda5c49e7920619eb3e1fd41
hatrari <ham.atrari@gmail.com>
2020-11-12 01:20:17 +0100

    add products service

---
+++ products-service/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace products_service.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+      [HttpGet]
+      public IEnumerable<string> Get()
+      {
+        return new string[] { "product 1", "product 2", "product 3" };
+      }
+    }
+}
